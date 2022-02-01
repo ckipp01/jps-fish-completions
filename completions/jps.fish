cd269bab4bf9de3feac3d8de24fee15c77725c08
Chris Kipp <ckipp@pm.me>
2022-02-01 08:56:07 +0100

    feat: don't forget help

--- completions/jps.fish
+++ completions/jps.fish
@@ -3,8 +3,9 @@
 ##################################################
 
 complete --command jps --no-files
-complete --command jps --short -q --description "Suppress the output of the class name, JAR file name, and arguments passed to the main method, producing only a list of local VM identifiers."
-complete --command jps --short -m --description "Output the arguments passed to the main method. The output may be null for embedded JVMs."
-complete --command jps --short -l --description "Output the full package name for the application's main class or the full path name to the application's JAR file."
-complete --command jps --short -v --description "Output the arguments passed to the JVM."
-complete --command jps --short -V --description "Output the arguments passed to the JVM through the flags file (the .hotspotrc file or the file specified by the -XX:Flags=<filename> argument)."
+complete --command jps --short q --description "Suppress the output of the class name, JAR file name, and arguments passed to the main method, producing only a list of local VM identifiers."
+complete --command jps --short m --description "Output the arguments passed to the main method. The output may be null for embedded JVMs."
+complete --command jps --short l --description "Output the full package name for the application's main class or the full path name to the application's JAR file."
+complete --command jps --short v --description "Output the arguments passed to the JVM."
+complete --command jps --short V --description "Output the arguments passed to the JVM through the flags file (the .hotspotrc file or the file specified by the -XX:Flags=<filename> argument)."
+complete --command jps --short h --long help --old help --description "Print the help message"
